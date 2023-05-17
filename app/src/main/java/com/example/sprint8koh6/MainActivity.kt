90cbbb7e8bc00acbecf7c72515545581286ab65a
Aleksei Anisimov <suxelshock@gmail.com>
2023-05-17 20:25:16 +0300

    Пример таймера

--- app/src/main/java/com/example/sprint8koh6/MainActivity.kt
+++ app/src/main/java/com/example/sprint8koh6/MainActivity.kt
@@ -2,13 +2,60 @@ package com.example.sprint8koh6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "SPRINT_8"
+    }
+
+    private var timerTv: TextView? = null
+    private var timerValue = 0
+    private var timer: Timer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "$this onCreate")
         setContentView(R.layout.activity_main)
+        timerTv = findViewById(R.id.timer)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "$this onStart")
+        timer = Timer().apply {
+            scheduleAtFixedRate(object : TimerTask() {
+                override fun run() {
+                    timerValue++
+                    Log.d(TAG, "$this tick timerValue=$timerValue")
+                    timerTv?.post {
+                        timerTv?.text = timerValue.toString()
+                    }
+                }
+            }, 1000L, 1000L)
+        }
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "$this onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "$this onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "$this onStop")
+        timer?.cancel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "$this onDestroy")
     }
 }
