f15d77533ef1e4bdde2bf70f48e876c8b3df9490
Aleksei Anisimov <suxelshock@gmail.com>
2023-05-17 20:14:01 +0300

    Примеры с ресурсами

--- app/src/main/java/com/example/sprint8koh6/MainActivity.kt
+++ app/src/main/java/com/example/sprint8koh6/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.sprint8koh6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
